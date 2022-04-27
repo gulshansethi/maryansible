2eb98a709109a2ff8a9f9ee80bc55dc6088bb881
LL\gulsethi <gulshan.sethi@publicissapient.com>
2022-04-27 12:40:47 +0530

    commit

--- variables.tf
+++ variables.tf
@@ -1,9 +1,69 @@
-variable "resource_group_name_prefix" {
-  default       = "rg"
-  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
+variable "create_resource_group" {
+  default = ""
+}
+variable "resourcegroup" {
+  default = ""
+}
+variable "secondaryresourcegroup" {
+  default = ""
+}
+variable "primarylocation" {
+  default = ""
+}
+variable "secondarylocation" {
+  default = ""
 }
 
-variable "resource_group_location" {
-  default = "eastus"
-  description   = "Location of the resource group."
+variable "sqlservername" {
+  default = ""
 }
+variable "sqlservername_secondary" {
+  default = ""
+}
+variable "databasename" {
+  default = ""
+}
+variable "databaseedition" {
+  default = ""
+}
+variable "sqldb_service_objective_name" {
+  default = ""
+}
+variable "enable_threat_detection_policy" {
+  default = "false"
+}
+variable "enable_sql_server_extended_auditing_policy" {
+  default = "false"
+}
+variable "enable_vulnerability_assessment" {
+  default = "false"
+}
+variable "enable_failover_group" {
+  default = "false"
+}
+variable "failover_group_name" {
+  default = ""
+}
+variable "enable_firewall_rules" {
+  default = ""
+}
+
+
+
+
+variable "ad_admin_login_name" {
+  default = ""
+}
+
+
+variable "keyvaultname" {
+  default = ""
+}
+
+variable "secretkey" {
+  default = ""
+}
+variable "secretvalue" {
+  default = ""
+}
+
